610c7e31dd8bce2ba41f331a2a6699fd41c21c7b
nancywen25 <nwen2011@gmail.com>
2020-04-16 21:18:09 -0400

    basic end-to-end tagger

--- name_tagger.py
+++ name_tagger.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import itertools
 
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
 def read_data(fname):
     """
     Given a file name of .pos-chunk or .pos-chunk-name format,
@@ -21,7 +25,6 @@ def read_data(fname):
     # split list into list of sentences
     sentences = [list(v) for k, v in itertools.groupby(lines, key=bool) if k]
 
-    # TODO: should we can drop the -DOCSTART- "sentences"?
     return sentences
 
 
@@ -44,8 +47,7 @@ def feature_builder(sentence):
     for token in sentence:
         d = {"token": token[0],
             "pos": token[1],
-            "chunk": token[2],
-            "tag": None}
+            "chunk": token[2]}
 
         if len(token) == 4: # includes the name tag
             d["tag"] = token[3]
@@ -55,7 +57,7 @@ def feature_builder(sentence):
     return basic_features
 
 
-def enhance_features():
+def extract_features(sentences):
     """
     Read in a .pos-chunk or .pos-chunk-name file,
     and augment with additional features
@@ -68,9 +70,106 @@ def enhance_features():
         basic_features = feature_builder(sentence)
         feature_list += basic_features
 
-    feature_df = pd.DataFrame(feature_list)
+    return pd.DataFrame(feature_list)
 
+def get_tags(sentences):
+    """
+    Given data with just token and tags,
+    returns the data as a dataframe with token and tag columns
 
+    Args:
+        sentences:
+
+    Returns:
+
+    """
+    tag_list = []
+    for sentence in sentences:
+        for token in sentence:
+            tag_list.append({'token': token[0],
+                             'tag': token[1]})
+    return pd.DataFrame(tag_list)
+
+def me_train(df):
+    """
+    Given a dataframe representing features and tag values,
+    trains a MaxEnt model to predict tag value
+
+    Args:
+        df: dataframe containing features and tag values
+
+    Returns:
+        lr: trained LogisticRegression moel
+        vec: trained feature vectorizer
+    """
+    # encode the features
+    vec = DictVectorizer()
+    features = ["token", "pos", "chunk"]
+    X_train = vec.fit_transform(df[features].to_dict("records")) # change df back to list of dicts
+    # vec.get_feature_names()
+
+    # encode the tag categories
+    y_train = df["tag"].values
+
+    # train the model
+    logreg = LogisticRegression(multi_class="multinomial",
+                                max_iter=1000)
+    lr = logreg.fit(X_train, y_train)
+    print("Accuracy:", lr.score(X_train, y_train)) # get in-sample accuracy
+
+    return lr, vec
+
+def me_tag(lr, vec, df):
+    features = ["token", "pos", "chunk"]
+    X = vec.transform(df[features].to_dict("records"))
+    y_pred = lr.predict(X)
+
+    return y_pred
+
+def write_data(fname_in, fname_out, df, y):
+    """
+    Write the predicted tags out to a .name file
+    Returns:
+
+    """
+    tokens = df['token'].values
+    i = 0
+    with open(fname_in, 'r') as infile:
+        with open(fname_out, 'w') as outfile:
+            for line in infile:
+                if not line: # empty line
+                    outfile.write('\n')
+                else:
+                    outfile.write(tokens[i] + "\t" + y[i] + "\n")
+                    i += 1
+
+# training model using train data
 fname_train = "data/CONLL_train.pos-chunk-name"
 sentences = read_data(fname_train)
+df = extract_features(sentences)
+lr, vec = me_train(df)
+
+# use model to label dev data
+fname_dev = "data/CONLL_dev.pos-chunk"
+fname_dev_tag = "data/CONLL_dev.name"
+sentences_dev = read_data(fname_dev)
+df_dev = extract_features(sentences_dev)
+
+tag_dev = read_data(fname_dev_tag)
+df_tags = get_tags(tag_dev)
+
+y_pred = me_tag(lr, vec, df_dev)
+
+# write predictions to file
+write_data("data/CONLL_dev.pos-chunk", "output/CONLL_dev.name", df_dev, y_pred)
+
+# evaluating dev performance
+y_true = df_dev["tag"].values
+print(accuracy_score(y_true, y_pred))
+
+
+
+
+
+
 
